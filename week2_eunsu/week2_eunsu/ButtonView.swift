1e9863458617d894b3376c353d45a289d4c4e5b3
Eunice <jes.e.0927@gmail.com>
2024-04-23 17:45:01 +0900

    Feat: 재사용 가능한 ButtonView 생성
    버튼 클릭에 따른 색상과 텍스트 변경 필요

--- week2_eunsu/week2_eunsu/ButtonView.swift
+++ week2_eunsu/week2_eunsu/ButtonView.swift
@@ -5,4 +5,22 @@
 //  Created by Eunsu JEONG on 4/23/24.
 //
 
-import Foundation
+import SwiftUI
+
+//TODO: 버튼 클릭에 따른 색상과 텍스트 변경 필요
+struct ButtonView: View {
+    let buttonText: String
+    let textColor: Color
+    let backgroundColor: Color
+    
+    var body: some View {
+        Button {
+            
+        } label: {
+            Text(buttonText)
+                .font(.buttonText)
+        }
+        .buttonStyle(ActionButtonStyle(textColor: textColor,
+                                       backgroundColor: backgroundColor))
+    }
+}
