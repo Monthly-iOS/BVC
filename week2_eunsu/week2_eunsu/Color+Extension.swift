8d19539dfc83b6ff64b5c225a4bbbe95095a4df7
Eunice <jes.e.0927@gmail.com>
2024-04-23 13:40:24 +0900

    ADD: 배경색 Color Extension 추가

---
+++ week2_eunsu/week2_eunsu/Color+Extension.swift
@@ -0,0 +1,12 @@
+//
+//  Color+Extension.swift
+//  week2_eunsu
+//
+//  Created by Eunsu JEONG on 4/23/24.
+//
+
+import SwiftUI
+
+extension Color {
+    static let backgroundColor = Color.black
+}
