6d675c8313b657501a0a6c2aca84e1450f9a258e
Eunice <jes.e.0927@gmail.com>
2024-04-23 17:35:23 +0900

    Feat: 버튼 폰트 추가

--- week2_eunsu/week2_eunsu/Font+Extension.swift
+++ week2_eunsu/week2_eunsu/Font+Extension.swift
@@ -8,5 +8,6 @@
 import SwiftUI
 
 extension Font {
-    static let clockText: Font = .system(size: 30, weight: .regular, design: .default)
+    static let clockText: Font = .system(size: 35, weight: .regular, design: .default)
+    static let buttonText: Font = .system(size: 20, weight: .regular, design: .default)
 }
