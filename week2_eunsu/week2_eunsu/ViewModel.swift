added78247fd94995fc533ea6ca8c380ed1dd574
Eunice <jes.e.0927@gmail.com>
2024-04-23 18:25:48 +0900

    Add: Model, ViewModel 생성

---
+++ week2_eunsu/week2_eunsu/ViewModel.swift
@@ -0,0 +1,22 @@
+//
+//  ViewModel.swift
+//  week2_eunsu
+//
+//  Created by Eunsu JEONG on 4/23/24.
+//
+
+import Foundation
+
+class ViewModel: ObservableObject {
+    @Published var presenters: [LapItemPresenter] = []
+    
+    init() {
+        self.presenters = [
+            LapItemPresenter(lap: "Lap 1", time: "00:02,85", type: .normal),
+            LapItemPresenter(lap: "Lap 2", time: "00:03,32", type: .normal),
+            LapItemPresenter(lap: "Lap 3", time: "00:01,80", type: .best),
+            LapItemPresenter(lap: "Lap 4", time: "00:05,17", type: .worst),
+            LapItemPresenter(lap: "Lap 5", time: "00:04,24", type: .normal)
+        ]
+    }
+}
