7f83c33fdf56a8819a4ddc56bee77521e83a7cef
Eunice <jes.e.0927@gmail.com>
2024-04-23 17:35:15 +0900

    Feat: 버튼 배경과 텍스트 색상 추가

--- week2_eunsu/week2_eunsu/Color+Extension.swift
+++ week2_eunsu/week2_eunsu/Color+Extension.swift
@@ -12,4 +12,12 @@ extension Color {
     static let clockHighlightedcolor = Color.white
     static let clockNormalColor = Color(red: 71/255, green: 70/255, blue: 74/255)
     static let clockTextColor = Color.white
+    static let startButtonColor = Color.green.opacity(0.4)
+    static let startButtonTextColor = Color.green
+    static let endButtonColor = Color.red.opacity(0.4)
+    static let endButtonTextColor = Color.red
+    static let lapButtonColor = Color.gray.opacity(0.4)
+    static let lapButtonTextColor = Color.gray
+    static let restartButtonColor = Color.gray
+    static let restartButtonTextColor = Color.white
 }
