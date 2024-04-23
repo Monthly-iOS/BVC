0226409588c53ed6af783a48b3871864c7c487b7
Eunice <jes.e.0927@gmail.com>
2024-04-23 16:42:45 +0900

    Feat: 초 글씨 크기 지정

--- week2_eunsu/week2_eunsu/Font+Extension.swift
+++ week2_eunsu/week2_eunsu/Font+Extension.swift
@@ -5,4 +5,8 @@
 //  Created by Eunsu JEONG on 4/23/24.
 //
 
-import Foundation
+import SwiftUI
+
+extension Font {
+    static let clockText: Font = .system(size: 30, weight: .regular, design: .default)
+}
