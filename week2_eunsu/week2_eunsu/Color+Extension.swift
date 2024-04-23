b8836ee5810588dcf65760a206aca8b198cb6095
Eunice <jes.e.0927@gmail.com>
2024-04-23 16:43:44 +0900

    Feat: 초 텍스트 색상 추가

--- week2_eunsu/week2_eunsu/Color+Extension.swift
+++ week2_eunsu/week2_eunsu/Color+Extension.swift
@@ -11,4 +11,5 @@ extension Color {
     static let backgroundColor = Color.black
     static let clockHighlightedcolor = Color.white
     static let clockNormalColor = Color(red: 71/255, green: 70/255, blue: 74/255)
+    static let clockTextColor = Color.white
 }
