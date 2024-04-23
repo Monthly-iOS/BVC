33f11cf94d1c558d1ee5d4f59498f797c883a536
Eunice <jes.e.0927@gmail.com>
2024-04-23 13:37:55 +0900

    ADD: .gitignore 적용

---
+++ week2_eunsu/week2_eunsu/week2_eunsuApp.swift
@@ -0,0 +1,17 @@
+//
+//  week2_eunsuApp.swift
+//  week2_eunsu
+//
+//  Created by Eunsu JEONG on 4/23/24.
+//
+
+import SwiftUI
+
+@main
+struct week2_eunsuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
