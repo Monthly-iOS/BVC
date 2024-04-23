92d58f82ec7ba964109e8040e09831c7a5552ace
Eunice <jes.e.0927@gmail.com>
2024-04-23 17:34:58 +0900

    Feat: 버튼 스타일 지정

--- week2_eunsu/week2_eunsu/ActionButtonStyle.swift
+++ week2_eunsu/week2_eunsu/ActionButtonStyle.swift
@@ -5,4 +5,20 @@
 //  Created by Eunsu JEONG on 4/23/24.
 //
 
-import Foundation
+import SwiftUI
+
+struct ActionButtonStyle: ButtonStyle {
+    let textColor: Color
+    let backgroundColor: Color
+    
+    func makeBody(configuration: Configuration) -> some View {
+        configuration.label
+            .frame(width: 100, height: 100)
+            .background(
+                Circle()
+                    .foregroundStyle(backgroundColor)
+                    .padding(4)
+            )
+            .foregroundStyle(textColor)
+    }
+}
