eff4552dfb85c18833e461cb7b28cd72dae7a2f4
Eunice <jes.e.0927@gmail.com>
2024-04-23 14:02:14 +0900

    ADD: 재사용 가능한 ClockView와 TickShape 파일 생성

---
+++ week2_eunsu/week2_eunsu/ClockView.swift
@@ -0,0 +1,14 @@
+//
+//  ClockView.swift
+//  week2_eunsu
+//
+//  Created by Eunsu JEONG on 4/23/24.
+//
+
+import SwiftUI
+
+struct ClockView: View {
+    var body: some View {
+        /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Hello, world!@*/Text("Hello, world!")/*@END_MENU_TOKEN@*/
+    }
+}
