ba1220463b67825ebc3f14fcad62c41928141d7c
Eunice <jes.e.0927@gmail.com>
2024-04-23 19:00:39 +0900

    Add: NumberTimer와 ClockTimer를 새로운 뷰 파일로 분리

---
+++ week2_eunsu/week2_eunsu/ClockTimerView.swift
@@ -0,0 +1,53 @@
+//
+//  ClockTimerView.swift
+//  week2_eunsu
+//
+//  Created by Eunsu JEONG on 4/23/24.
+//
+
+import SwiftUI
+
+struct ClockTimerView: View {
+    let width: CGFloat
+    let numberWidth: CGFloat
+    private let tickHeight: CGFloat = 8
+    private let longTickHeight: CGFloat = 12
+    private let tickWidth: CGFloat = 2
+    
+    var body: some View {
+        ZStack {
+            ClockView(count: 240,
+                      longDivider: 4,
+                      longTickHeigh: longTickHeight,
+                      tickHeight: tickHeight,
+                      tickWidth: tickWidth,
+                      highlightedColorDivider: 20,
+                      highlightedColor: .clockHighlightedcolor,
+                      normalColor: .clockNormalColor)
+            .frame(width: width, height: width)
+            
+            NumbersView(numbers: getNumbers(count: 12),
+                        font: .clockText,
+                        textColor: .clockTextColor)
+            .frame(width: numberWidth, height: numberWidth)
+            
+            NeedleView(width: 8,
+                       height: width,
+                       color: .needleNormalColor,
+                       bottomLineHeight: 30)
+            .rotationEffect(.radians(Double.pi / 2))
+        }
+    }
+    
+    private func getNumbers(count: Int) -> [Int] {
+        var numbers: [Int] = []
+        
+        numbers.append(count * 5)
+        
+        for index in 1..<count {
+            numbers.append(index * 5)
+        }
+        
+        return numbers
+    }
+}
---
+++ week2_eunsu/week2_eunsu/NumberTimerView.swift
@@ -0,0 +1,15 @@
+//
+//  NumberTimerView.swift
+//  week2_eunsu
+//
+//  Created by Eunsu JEONG on 4/23/24.
+//
+
+import SwiftUI
+
+struct NumberTimerView: View {
+    var body: some View {
+        /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Hello, world!@*/Text("Hello, world!")/*@END_MENU_TOKEN@*/
+    }
+}
+
