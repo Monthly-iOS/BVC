61aa6a96cfac41678c5a45b1397fadea6e8a7109
Eunice <jes.e.0927@gmail.com>
2024-04-25 21:24:54 +0900

    Feat: 미니 시계 폰트 설정

--- week2_eunsu/week2_eunsu/Font+Extension.swift
+++ week2_eunsu/week2_eunsu/Font+Extension.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 extension Font {
     static let clockText: Font = .system(size: 35, weight: .regular, design: .default)
+    static let clockMiniText: Font = .system(size: 17, weight: .regular, design: .default)
     static let buttonText: Font = .system(size: 20, weight: .regular, design: .default)
     static let lapListText: Font = .system(size: 17, weight: .regular, design: .default)
     static let lapClockText: Font = .system(size: 22, weight: .light, design: .default)
