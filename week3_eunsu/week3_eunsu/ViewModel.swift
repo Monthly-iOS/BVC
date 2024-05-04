6abf0ffe68b4b109bc44da8da33aa5f456702d26
Eunice <jes.e.0927@gmail.com>
2024-05-04 23:07:48 +0900

    Feat: 이미지 url을 받아 비동기로 uiimage를 return하는 함수 생성

--- week3_eunsu/week3_eunsu/ViewModel.swift
+++ week3_eunsu/week3_eunsu/ViewModel.swift
@@ -5,4 +5,27 @@
 //  Created by Eunsu JEONG on 5/4/24.
 //
 
-import Foundation
+import UIKit
+
+final class ViewModel {
+    func fetchImage(url: String) async throws -> UIImage {
+        let url = URL(string: url)
+        
+        guard let url = url else {
+            throw NSError(domain: "url error", code: 888)
+        }
+        
+        let request = URLRequest(url: url)
+        let (data, response) = try await URLSession.shared.data(for: request)
+            
+        guard let statusCode = (response as? HTTPURLResponse)?.statusCode, (200...299).contains(statusCode) else {
+            throw NSError(domain: "fetch error", code: 1004)
+        }
+        
+        guard let image = UIImage(data: data) else {
+            throw NSError(domain: "image coverting error", code: 999)
+        }
+
+        return image
+    }
+}
