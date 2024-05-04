51870c964d39f5c4922453bfcce4113dbb6e6a22
Eunice <jes.e.0927@gmail.com>
2024-05-04 21:25:39 +0900

    Feat: HomeVC에 테스트 데이터로 TableView 설정

--- week3_eunsu/week3_eunsu/HomeViewController.swift
+++ week3_eunsu/week3_eunsu/HomeViewController.swift
@@ -8,10 +8,43 @@
 import UIKit
 
 class HomeViewController: UIViewController {
-
+    private let homeFeedTable: UITableView = UITableView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        setTable()
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        homeFeedTable.frame = view.bounds
+    }
+    
+    private func setTable() {
+        homeFeedTable.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        view.addSubview(homeFeedTable)
+        homeFeedTable.delegate = self
+        homeFeedTable.dataSource = self
+    }
+}
 
-        // Do any additional setup after loading the view.
+extension HomeViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 20
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = "Hello!"
+        return cell
+    }
+    
+    //각 셀의 높이
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 200
+    }
+    
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 40
     }
 }
