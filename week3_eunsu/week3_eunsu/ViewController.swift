6fb872d1633ab44c231dc500943c2235f15e5bf7
Eunice <jes.e.0927@gmail.com>
2024-05-04 20:31:59 +0900

    Feat: 프로젝트 생성 및 General 설정 Set up

---
+++ week3_eunsu/week3_eunsu/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  week3_eunsu
+//
+//  Created by Eunsu JEONG on 5/4/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
