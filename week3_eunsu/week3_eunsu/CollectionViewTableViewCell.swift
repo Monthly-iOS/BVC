400a4c34ecb514c8c65c3068c96b9f1ceeaee714
Eunice <jes.e.0927@gmail.com>
2024-05-04 22:02:55 +0900

    Feat: Cell에 CollectionView를 설정

--- week3_eunsu/week3_eunsu/CollectionViewTableViewCell.swift
+++ week3_eunsu/week3_eunsu/CollectionViewTableViewCell.swift
@@ -10,12 +10,43 @@ import UIKit
 ///TableView의 각 행마다 CollectionView가 들어감
 class CollectionViewTableViewCell: UITableViewCell {
     static let identifier = String(describing: CollectionViewTableViewCell.self)
+    private let flowLayout: UICollectionViewFlowLayout = UICollectionViewFlowLayout()
+    private lazy var collectionView: UICollectionView = UICollectionView(frame: .zero, collectionViewLayout: flowLayout)
     
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
+        setCollectionView()
     }
     
     required init?(coder: NSCoder) {
         fatalError()
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        collectionView.frame = contentView.bounds
+    }
+    
+    private func setCollectionView() {
+        flowLayout.scrollDirection = .horizontal
+        flowLayout.itemSize = CGSize(width: 140, height: 200)
+        
+        contentView.addSubview(collectionView)
+
+        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "cell")
+        collectionView.delegate = self
+        collectionView.dataSource = self
+    }
+}
+
+extension CollectionViewTableViewCell: UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
+        cell.backgroundColor = .purple
+        return cell
     }
 }
