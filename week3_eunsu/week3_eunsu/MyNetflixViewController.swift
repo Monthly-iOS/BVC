1e564a38d486b07119802c604620ba57027ec6c4
Eunice <jes.e.0927@gmail.com>
2024-05-04 21:10:49 +0900

    Add: 탭바 별 VC 파일 생성

---
+++ week3_eunsu/week3_eunsu/MyNetflixViewController.swift
@@ -0,0 +1,17 @@
+//
+//  MyNetflixViewController.swift
+//  week3_eunsu
+//
+//  Created by Eunsu JEONG on 5/4/24.
+//
+
+import UIKit
+
+class MyNetflixViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
---
+++ week3_eunsu/week3_eunsu/NewAndHotViewController.swift
@@ -0,0 +1,17 @@
+//
+//  NewAndHotViewController.swift
+//  week3_eunsu
+//
+//  Created by Eunsu JEONG on 5/4/24.
+//
+
+import UIKit
+
+class NewAndHotViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
