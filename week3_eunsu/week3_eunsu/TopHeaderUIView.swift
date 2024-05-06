3a87cc110b10082d0a1c90613e8ad2bf3e3a1200
Eunice <jes.e.0927@gmail.com>
2024-05-06 17:17:33 +0900

    Feat: 재생, 내가 찜한 리스트 버튼 생성

--- week3_eunsu/week3_eunsu/TopHeaderUIView.swift
+++ week3_eunsu/week3_eunsu/TopHeaderUIView.swift
@@ -10,12 +10,15 @@ import UIKit
 class TopHeaderUIView: UIView {
     private var viewModel: ViewModel
     private let topImageView: UIImageView = UIImageView()
+    private let playButton: UIButton = UIButton()
+    private let downloadButton: UIButton = UIButton()
     
     init(frame: CGRect, viewModel: ViewModel) {
         self.viewModel = viewModel
         super.init(frame: frame)
         setTopHeader()
         addGradient()
+        setButtons()
     }
     
     required init?(coder: NSCoder) {
@@ -25,6 +28,41 @@ class TopHeaderUIView: UIView {
     override func layoutSubviews() {
         super.layoutSubviews()
         topImageView.frame = bounds
+    }
+    
+    private func setButtons() {
+        addSubview(playButton)
+        addSubview(downloadButton)
+        
+        playButton.setTitle("  재생", for: .normal)
+        playButton.setImage(UIImage(systemName: "play.fill"), for: .normal)
+        playButton.setTitleColor(.black, for: .normal)
+        playButton.tintColor = .black
+        playButton.titleLabel?.font = .systemFont(ofSize: 14)
+        playButton.layer.backgroundColor = UIColor.white.cgColor
+        playButton.layer.cornerRadius = 3
+        playButton.translatesAutoresizingMaskIntoConstraints = false
+        
+        downloadButton.setTitle("  내가 찜한 리스트", for: .normal)
+        downloadButton.setImage(UIImage(systemName: "plus"), for: .normal)
+        downloadButton.setTitleColor(.white, for: .normal)
+        downloadButton.tintColor = .white
+        downloadButton.titleLabel?.font = .systemFont(ofSize: 13)
+        downloadButton.layer.backgroundColor = UIColor.systemGray6.cgColor
+        downloadButton.layer.cornerRadius = 3
+        downloadButton.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            playButton.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 40),
+            playButton.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -40),
+            playButton.widthAnchor.constraint(equalToConstant: frame.width / 2.6),
+            playButton.heightAnchor.constraint(equalToConstant: 40),
+            
+            downloadButton.leadingAnchor.constraint(equalTo: centerXAnchor, constant: 10),
+            downloadButton.bottomAnchor.constraint(equalTo: playButton.bottomAnchor),
+            downloadButton.widthAnchor.constraint(equalToConstant: frame.width / 2.6),
+            downloadButton.heightAnchor.constraint(equalToConstant: 40)
+        ])
     }
     
     private func addGradient() {
