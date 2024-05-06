008f6621b88e59ae1b72a032e134f32233157ea2
Eunice <jes.e.0927@gmail.com>
2024-05-06 23:37:25 +0900

    Feat: API에서 Decoding된 데이터에서 실제 뷰에 사용할 데이터만 분리한 모델 생성

---
+++ week3_eunsu/week3_eunsu/NetflixItem.swift
@@ -0,0 +1,14 @@
+//
+//  NetflixItem.swift
+//  week3_eunsu
+//
+//  Created by Eunsu JEONG on 5/6/24.
+//
+
+import Foundation
+
+struct NetflixItem {
+    var rank: String?
+    let title: String
+    let image: String
+}
