1c6a0bb66808ce47a493fa0b8f7aca9769961f94
Eunice <jes.e.0927@gmail.com>
2024-05-06 23:44:25 +0900

    Feat: 실제 CollectionViewCell에 들어갈 Cell 생성

--- week3_eunsu/week3_eunsu/TitleCollectionViewCell.swift
+++ week3_eunsu/week3_eunsu/TitleCollectionViewCell.swift
@@ -8,5 +8,33 @@
 import UIKit
 
 class TitleCollectionViewCell: UICollectionViewCell {
+    static let identifier = String(describing: TitleCollectionViewCell.self)
+    private let posterImageView: UIImageView = UIImageView()
     
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setUI()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        posterImageView.frame = contentView.bounds
+    }
+    
+    private func setUI() {
+        contentView.addSubview(posterImageView)
+        posterImageView.contentMode = .scaleAspectFill
+    }
+    
+    func configure(with imageURL: String) {
+        Task {
+            do {
+                posterImageView.image = try await tmdbAPI.shared.fetchImage(url: imageURL)
+            }
+        }
+    }
 }
