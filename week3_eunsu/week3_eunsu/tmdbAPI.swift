b4e3ae1e0e503ae2249e088fd54fa0055a7aca01
Eunice <jes.e.0927@gmail.com>
2024-05-06 23:41:27 +0900

    Feat: fetchImage 함수를 tmdbAPI 클래스에 추가

--- week3_eunsu/week3_eunsu/tmdbAPI.swift
+++ week3_eunsu/week3_eunsu/tmdbAPI.swift
@@ -93,4 +93,25 @@ class tmdbAPI {
         let data = try await requestData(url: url, queryItems: queryItems)
         print(String(decoding: data, as: UTF8.self))
     }
+    
+    func fetchImage(url: String) async throws -> UIImage {
+        let url = URL(string: url)
+        
+        guard let url = url else {
+            throw NSError(domain: "url error", code: 888)
+        }
+        
+        let request = URLRequest(url: url)
+        let (data, response) = try await URLSession.shared.data(for: request)
+            
+        guard let statusCode = (response as? HTTPURLResponse)?.statusCode, (200...299).contains(statusCode) else {
+            throw NSError(domain: "fetch error", code: 1004)
+        }
+        
+        guard let image = UIImage(data: data) else {
+            throw NSError(domain: "image coverting error", code: 999)
+        }
+
+        return image
+    }
 }
