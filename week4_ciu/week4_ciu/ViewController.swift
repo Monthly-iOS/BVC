79fcb927886ee4a12def06fd7149b284838ccb12
kangciu <a2849535@gmail.com>
2024-05-08 08:12:23 +0900

    feat; 프로젝트 초기 세팅 및 스토리보드 삭제

---
+++ week4_ciu/week4_ciu/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  week4_ciu
+//
+//  Created by 강치우 on 5/8/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
