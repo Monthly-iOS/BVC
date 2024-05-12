41e4c486a2b63adcc6aa0a089329ef80ca6505cc
Hminchae <hminchae@gmail.com>
2024-05-13 01:23:21 +0900

    ADD : API_KEY 공백 처리

---
+++ week3_minchae/Netflix/Netflix/Managers/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constraints.swift
+//  Netflix
+//
+//  Created by 황민채 on 5/12/24.
+//
+
+import Foundation
+
+struct Constants {
+    static let API_KEY = ""
+    static let baseURL = "https://api.themoviedb.org"
+}
