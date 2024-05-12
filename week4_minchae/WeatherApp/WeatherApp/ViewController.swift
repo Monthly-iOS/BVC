1bd2a730718df1e70b0facc8013c03b479c9d893
Hminchae <hminchae@gmail.com>
2024-05-13 02:10:39 +0900

    ADD : 최초 셋팅

---
+++ week4_minchae/WeatherApp/WeatherApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WeatherApp
+//
+//  Created by 황민채 on 5/13/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBlue
+    }
+
+
+}
+
