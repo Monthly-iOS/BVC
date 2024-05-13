305bf0a6b83f2c142cd653558041821aa7f20947
Minyoung Lee <mminy62@gmail.com>
2024-05-14 02:09:12 +0900

    Init: 프로젝트 생성 및 이미지 자료 추가

---
+++ week4_min0/week4_min0/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  week4_min0
+//
+//  Created by 이민영 on 5/12/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
