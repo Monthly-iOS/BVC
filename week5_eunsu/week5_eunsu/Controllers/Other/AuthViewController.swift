3cdc762e76e708b0ccb41618d89d985c96131ab2
ios-jeongeunsu <jeong.eunsu@cashwalk.io>
2024-05-29 12:30:06 +0900

    Feat: 로그인 버튼 클릭하면 AuthVC로 이동, 웹뷰 내장

--- week5_eunsu/week5_eunsu/Controllers/Other/AuthViewController.swift
+++ week5_eunsu/week5_eunsu/Controllers/Other/AuthViewController.swift
@@ -6,24 +6,38 @@
 //
 
 import UIKit
+import WebKit
 
 class AuthViewController: UIViewController {
-
+    private let webView: WKWebView = {
+        let prefs = WKWebpagePreferences()
+        prefs.allowsContentJavaScript = true
+        let config = WKWebViewConfiguration()
+        config.defaultWebpagePreferences = prefs
+        let webView = WKWebView(frame: .zero, configuration: config)
+        
+        return webView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        title = "Sign In"
+        webView.navigationDelegate = self
+        setWebView()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func setWebView() {
+        view.addSubview(webView)
+        webView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            webView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            webView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            webView.topAnchor.constraint(equalTo: view.topAnchor),
+            webView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
     }
-    */
+}
 
+extension AuthViewController: WKNavigationDelegate {
+    
 }
