039fdfe4026b31cfa09e1b4f72bd52dea4c32429
Eunice <jes.e.0927@gmail.com>
2024-05-29 20:58:10 +0900

    Feat: 웹뷰로 navigate하는 버튼 구현

--- week5_eunsu/week5_eunsu/Controllers/Other/WelcomeViewController.swift
+++ week5_eunsu/week5_eunsu/Controllers/Other/WelcomeViewController.swift
@@ -8,11 +8,46 @@
 import UIKit
 
 class WelcomeViewController: UIViewController {
+    private let signInButton = UIButton()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         title = "Spotify"
         view.backgroundColor = .systemGreen
+        
+        setSignInButton()
+    }
+    
+    private func setSignInButton() {
+        view.addSubview(signInButton)
+        
+        signInButton.backgroundColor = .white
+        signInButton.setTitleColor(.black, for: .normal)
+        signInButton.setTitle("Sign in with Spotify", for: .normal)
+        
+        signInButton.addTarget(self, action: #selector(didTapSignIn), for: .touchUpInside)
+        
+        signInButton.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            signInButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            signInButton.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -50),
+            signInButton.widthAnchor.constraint(equalToConstant: view.bounds.width - 40),
+            signInButton.heightAnchor.constraint(equalToConstant: 50)
+        ])
+    }
+    
+    @objc
+    func didTapSignIn() {
+        let vc = AuthViewController()
+        vc.compleationHandler = { [weak self] success in
+            self?.handleSignIn(success: success)
+        }
+        vc.navigationItem.largeTitleDisplayMode = .never
+        navigationController?.pushViewController(vc, animated: true)
+    }
+    
+    private func handleSignIn(success: Bool) {
+        //Log user in or yell at them for error
     }
 }
