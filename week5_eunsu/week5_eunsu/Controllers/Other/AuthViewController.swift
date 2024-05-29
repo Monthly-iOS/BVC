035b3d2eaf4bfaafcd8eac89f0947146afc4b941
Eunice <jes.e.0927@gmail.com>
2024-05-29 20:57:37 +0900

    Feat: 로그인 할 수 있는 WebView 연결

--- week5_eunsu/week5_eunsu/Controllers/Other/AuthViewController.swift
+++ week5_eunsu/week5_eunsu/Controllers/Other/AuthViewController.swift
@@ -6,24 +6,52 @@
 //
 
 import UIKit
+import WebKit
 
 class AuthViewController: UIViewController {
-
+    private let webView: WKWebView = {
+        let prefs = WKWebpagePreferences()
+        prefs.allowsContentJavaScript = true
+        let config = WKWebViewConfiguration()
+        config.defaultWebpagePreferences = prefs
+        let webView = WKWebView(frame: .zero, configuration: config)
+        
+        return webView
+    }()
+    
+    public var compleationHandler: ((Bool) -> Void)?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        setWebView()
+        connectWebView()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func setWebView() {
+        view.addSubview(webView)
+        webView.navigationDelegate = self
+        webView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            webView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            webView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            webView.topAnchor.constraint(equalTo: view.topAnchor),
+            webView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
     }
-    */
+    
+    private func connectWebView() {
+        guard let url = AuthManager.shared.signInURL else { return }
+        webView.load(URLRequest(url: url))
+    }
+}
 
+extension AuthViewController: WKNavigationDelegate {
+    func webView(_ webView: WKWebView, didStartProvisionalNavigation navigation: WKNavigation!) {
+        guard let url = webView.url else { return }
+        
+        //Exchange the code for access token
+        guard let code = URLComponents(string: url.absoluteString)?.queryItems?.first(where: { $0.name == "code"})?.value else { return }
+        
+        print("Code: \(code)")
+    }
 }
