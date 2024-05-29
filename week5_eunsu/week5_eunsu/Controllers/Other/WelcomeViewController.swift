cef366fc2d0667705302e73e6e8908a5360aaeae
ios-jeongeunsu <jeong.eunsu@cashwalk.io>
2024-05-29 12:29:38 +0900

    Feat: WelcomeVC에 로그인 버튼 생성

--- week5_eunsu/week5_eunsu/Controllers/Other/WelcomeViewController.swift
+++ week5_eunsu/week5_eunsu/Controllers/Other/WelcomeViewController.swift
@@ -8,11 +8,37 @@
 import UIKit
 
 class WelcomeViewController: UIViewController {
-
+    private let signInButton = UIButton()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         title = "Spotify"
         view.backgroundColor = .systemGreen
+        setButton()
+    }
+    
+    private func setButton() {
+        view.addSubview(signInButton)
+        
+        signInButton.backgroundColor = .white
+        signInButton.setTitle("Sign In with Spotify", for: .normal)
+        signInButton.setTitleColor(.black, for: .normal)
+        
+        signInButton.addTarget(self, action: #selector(didTapSignIn), for: .touchUpInside)
+        
+        signInButton.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            signInButton.widthAnchor.constraint(equalToConstant: view.bounds.width - 40),
+            signInButton.heightAnchor.constraint(equalToConstant: 50),
+            signInButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            signInButton.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -50)
+        ])
+    }
+    
+    @objc func didTapSignIn() {
+        let vc = AuthViewController()
+        vc.navigationItem.largeTitleDisplayMode = .never
+        navigationController?.pushViewController(vc, animated: true)
     }
 }
