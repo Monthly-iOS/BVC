5c760b7235f0a7c24c0305704d0c017c866484b3
Eunice <jes.e.0927@gmail.com>
2024-06-19 19:35:13 +0900

    Feat: 로그인 성공시 뷰 전환, 실패시 alert 노출 구현

--- week5_eunsu/week5_eunsu/Controllers/Other/WelcomeViewController.swift
+++ week5_eunsu/week5_eunsu/Controllers/Other/WelcomeViewController.swift
@@ -50,5 +50,17 @@ class WelcomeViewController: UIViewController {
     
     private func handleSignIn(success: Bool) {
         //Log user in or yell at them for error
+        guard success else {
+            let alert = UIAlertController(title: "Oops",
+                                          message: "Something went wrong when signing in",
+                                          preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "Dismiss", style: .cancel))
+            present(alert, animated: true)
+            return
+        }
+        
+        let mainAppTabBarVC = TabBarTableViewController()
+        mainAppTabBarVC.modalPresentationStyle = .fullScreen
+        present(mainAppTabBarVC, animated: true)
     }
 }
